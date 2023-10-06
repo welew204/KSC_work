ac9d5ff986574f597efc54b4068af81b8e0b6443
Welew <will@willbelew.com>
2023-10-06 12:26:36 -0700

    done through 2.1.3

--- frontend/2.1-javascript/activities/2_farmfeeder/js/main.js
+++ frontend/2.1-javascript/activities/2_farmfeeder/js/main.js
@@ -1,3 +1,33 @@
 // JavaScript code goes here!
+console.log(
+  "computers suck, but this file just ran! ...",
+  "frontend/2.1-javascript/activities/2_farmfeeder/js/main.js"
+);
+let horseFedCount = 0;
+const horseSpan = document.createElement("span");
+let body = document.querySelector("body");
+function feedHorse() {
+  console.log("Horse is getting fed.");
+  if (horseFedCount < 10) {
+    horseFedCount++; // increment horseFedCount
+    horseSpan.textContent = "Horse got fed this many times:" + horseFedCount;
+    body.appendChild(horseSpan);
+  } else {
+    let horseDone = document.createElement("h1");
+    horseDone.textContent = "I'm FULLLL! Stop feeding me.";
+    body.append(horseDone);
+  }
+}
+let goatFedCount = 0;
+const goatSpan = document.createElement("span");
 
-
+function feedGoat() {
+  console.log("Goat is getting fed.");
+  goatFedCount++; // increment goatFedCount
+  goatSpan.textContent = "Goat got fed this many times:" + goatFedCount;
+  body.appendChild(goatSpan);
+  if (goatFedCount > 10) {
+    horseFedCount = Math.floor(horseFedCount / 2);
+    horseSpan.textContent = "Horse got fed this many times:" + horseFedCount;
+  }
+}
--- frontend/2.1-javascript/activities/3_todo_app/js/main.js
+++ frontend/2.1-javascript/activities/3_todo_app/js/main.js
@@ -1,59 +1,53 @@
 let todoItems = [
-    'Work on homework',
-    'Eat',
-    'Do more coding',
-    'Stay up late with even more coding',
+  "Work on homework",
+  "Eat",
+  "Do more coding",
+  "Stay up late with even more coding",
 ];
 
-
 function render() {
-    console.log('render getting invoked');
+  console.log("render getting invoked --> hi boo");
 
+  // Challenge #2 HINT:
 
-    // Challenge #2 HINT:
-    /*
-    let div = document.querySelector('...SOMETHING GOES HERE....');
-    console.log(div);
-    */
+  let div = document.querySelector("#todo-input");
+  div.value = "";
+  console.log(div);
 
+  // Challenge #3 HINT:
+  let list = document.querySelector("#todo-list");
+  list.innerHTML = "";
 
-    // Challenge #3 HINT:
-    /*
-    let paragraph = document.createElement('p');
-    paragraph.textContent = '...some string goes here....';
-    SOME_VARIABLE_NAME_GOES_HERE.appendChild(paragraph);
-    */
+  // Challenge #4 HINT:
 
-
-
-    // Challenge #4 HINT:
-    /*
-    for (let item of todoItems) {
-        console.log('for loop item:', item);
-    }
-    */
-
+  for (let item of todoItems) {
+    let paragraph = document.createElement("p");
+    console.log("for loop item:", item);
+    paragraph.textContent = item;
+    list.appendChild(paragraph);
+  }
 }
 
 function addItem() {
-    console.log('add item button getting clicked');
-    let input = document.querySelector('#todo-input');
-    console.log('The input element:', input);
-    let value = input.value;
-    console.log('The value entered:', value);
+  console.log("add item button getting clicked");
+  let input = document.querySelector("#todo-input");
+  console.log("The input element:", input);
+  let value = input.value;
+  console.log("The value entered:", value);
+  todoItems.push(value);
+  console.log("...DONE adding item to list....");
+  render();
 
-    // Challenge #6 HINT:
-    // todoItems.push('Testing, testing, 123...')
-    // console.log('To-do items after push:', todoItems);
+  // Challenge #6 HINT:
+  // todoItems.push('Testing, testing, 123...')
+  // console.log('To-do items after push:', todoItems);
 
+  // Challenge #7 HINT:
+  // Hint #1: To invoke the render function, use: render()
+  // Hint #2: Clear an element like: div.innerHTML = '';
 
-    // Challenge #7 HINT:
-    // Hint #1: To invoke the render function, use: render()
-    // Hint #2: Clear an element like: div.innerHTML = '';
-
-
-    // Challenge #8 HINT:
-    // Look at the console.log output with "value" and "input" for clues!
+  // Challenge #8 HINT:
+  // Look at the console.log output with "value" and "input" for clues!
 }
 
 render();
