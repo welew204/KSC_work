8b717439db733e54e1cbb2de703dc09c8fe5af29
Welew <will@willbelew.com>
2023-10-09 16:58:25 -0700

    done thru 2.3.5

--- frontend/2.3-api/activities/5_api_gif_search/js/main.js
+++ frontend/2.3-api/activities/5_api_gif_search/js/main.js
@@ -1,4 +1,13 @@
 // Do the activity here!
+console.log("loaded the ish");
 
+function doFetch() {
+  console.log("doing ze FETCH");
+  fetch("http://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 
-
+function displayOutput() {
+  console.log("displaying ze OUTPUT");
+}
