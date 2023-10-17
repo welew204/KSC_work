2465ff8ac1133b9aa58427daa7781047d10fd8d4
Welew <will@willbelew.com>
2023-10-17 10:56:57 -0700

    done with FE 3.3.2

--- frontend/3.3-lifecycle/activities/2_weather_useeffect/src/App.js
+++ frontend/3.3-lifecycle/activities/2_weather_useeffect/src/App.js
@@ -1,31 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [searchBox, setSearchBox] = useState('Oakland, CA');
+  let prevSearch = localStorage.getItem("weathersearch");
+  if (!prevSearch) {
+    prevSearch = "Oakland, California";
+  }
+  console.log("previous search was:", prevSearch);
+
+  const [searchBox, setSearchBox] = useState(prevSearch);
   const [weatherResult, setWeatherResult] = useState({
-    location: '',
-    temperature: '',
-    description: '',
-    windSpeed: '',
-    humidity: '',
-    pressure: '',
+    location: "",
+    temperature: "",
+    description: "",
+    windSpeed: "",
+    humidity: "",
+    pressure: "",
   });
 
   function doFetch() {
-    console.log('hitting refresh', searchBox);
+    //console.log("hitting refresh", searchBox);
     const query = searchBox;
-    const url = 'http://api.openweathermap.org/data/2.5/weather?q=' +
-      query + '&appid=0de82b6b4ba5d843dac44bbee4d02543';
+    const url =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      query +
+      "&appid=0de82b6b4ba5d843dac44bbee4d02543";
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log('receiving data', data);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("receiving data", data);
 
-        if (!data.main) { // 404
+        if (!data.main) {
+          // 404
           setWeatherResult({
-            ...weatherResult,
-            location: 'Not found.'
+            location: "Not found.",
           });
           return; // Exit function
         }
@@ -42,31 +50,51 @@ function App() {
       });
   }
 
+  useEffect(() => {
+    doFetch();
+    console.log("useEffect w/ []");
+  }, []);
+  useEffect(() => {
+    if (!searchBox) {
+    } else {
+      localStorage.setItem("weathersearch", searchBox);
+      console.log("searchBox stored: " + searchBox);
+    }
+  }, [searchBox]);
+  //console.log("rerendering");
+
   return (
-    <div className="App App--clear">
-      <div className="WeatherDashboard">
-        <div className="WeatherDashboard-location">
+    <div className='App App--clear'>
+      <div className='WeatherDashboard'>
+        <div className='WeatherDashboard-location'>
           {weatherResult.location}
         </div>
-        <div className="WeatherDashboard-overview">
-          <span className="WeatherDashboard-temperature">
+        <div className='WeatherDashboard-overview'>
+          <span className='WeatherDashboard-temperature'>
             {weatherResult.temperature}° <span>C</span>
           </span>
-          <div className="WeatherDashboard-description">{weatherResult.description}</div>
+          <div className='WeatherDashboard-description'>
+            {weatherResult.description}
+          </div>
         </div>
-        <div className="WeatherDashboard-details">
-          <div className="WeatherDashboard-label">Wind</div>
-          <div className="WeatherDashboard-data">weatherResult.windSpeed <span>km/h</span></div>
-          <div className="WeatherDashboard-label">Humidity</div>
-          <div className="WeatherDashboard-data">weatherResult.humidity <span>%</span></div>
-          <div className="WeatherDashboard-label">Pressure</div>
-          <div className="WeatherDashboard-data">weatherResult.pressure</div>
+        <div className='WeatherDashboard-details'>
+          <div className='WeatherDashboard-label'>Wind</div>
+          <div className='WeatherDashboard-data'>
+            {weatherResult.windSpeed} <span>km/h</span>
+          </div>
+          <div className='WeatherDashboard-label'>Humidity</div>
+          <div className='WeatherDashboard-data'>
+            {weatherResult.humidity} <span>%</span>
+          </div>
+          <div className='WeatherDashboard-label'>Pressure</div>
+          <div className='WeatherDashboard-data'>{weatherResult.pressure}</div>
         </div>
-        <div className="Controls">
+        <div className='Controls'>
           <input
-            placeholder="Enter location name"
+            placeholder='Enter location name'
             value={searchBox}
-            onChange={ev => setSearchBox(ev.target.value)} />
+            onChange={(ev) => setSearchBox(ev.target.value)}
+          />
           <button onClick={doFetch}>Refresh</button>
         </div>
       </div>
