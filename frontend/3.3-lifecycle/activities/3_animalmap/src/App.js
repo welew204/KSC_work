34b7b2c4553130e99fcfb7e0d949e0dd3005d7e0
Welew <will@willbelew.com>
2023-10-17 11:48:58 -0700

    done thru FE 3.3.3

--- frontend/3.3-lifecycle/activities/3_animalmap/src/App.js
+++ frontend/3.3-lifecycle/activities/3_animalmap/src/App.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 // Import our animal images
-import catImage from './images/cat.png';
-import cowImage from './images/cow.png';
-import dogImage from './images/dog.png';
-import pigImage from './images/pig.png';
-import ratImage from './images/rat.png';
+import catImage from "./images/cat.png";
+import cowImage from "./images/cow.png";
+import dogImage from "./images/dog.png";
+import pigImage from "./images/pig.png";
+import ratImage from "./images/rat.png";
 
 // Create a global list of animal types, used to populate the selector
 const animalTypes = [
-  {type: 'cat', imageSrc: catImage},
-  {type: 'cow', imageSrc: cowImage},
-  {type: 'dog', imageSrc: dogImage},
-  {type: 'pig', imageSrc: pigImage},
-  {type: 'rat', imageSrc: ratImage},
+  { type: "cat", imageSrc: catImage },
+  { type: "cow", imageSrc: cowImage },
+  { type: "dog", imageSrc: dogImage },
+  { type: "pig", imageSrc: pigImage },
+  { type: "rat", imageSrc: ratImage },
 ];
 
 function App() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [newAnimalInfo, setNewAnimalInfo] = useState({});
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [limitBy, setLimitBy] = useState(false);
   const [animals, setAnimals] = useState([
-    {name: 'whiskers', image: catImage, type: 'cat'},
-    {name: 'doggerino', image: dogImage, type: 'dog'},
-    {name: 'mickey', image: ratImage, type: 'rat'},
+    { name: "whiskers", image: catImage, type: "cat" },
+    { name: "doggerino", image: dogImage, type: "dog" },
+    { name: "mickey", image: ratImage, type: "rat" },
   ]);
 
   function onNameChange(ev) {
@@ -34,13 +34,15 @@ function App() {
   }
 
   function addAnimal() {
+    console.log(animals);
     // Create a new object to represent the animal that we are adding
     const newAnimal = {
       name: name,
       image: newAnimalInfo.imageSrc,
       type: newAnimalInfo.type,
     };
-    setAnimals([ // Add that object to the end of the array
+    setAnimals([
+      // Add that object to the end of the array
       ...animals, // the "..." means include the previous contents of the array
       newAnimal, // and this means include the new object
     ]);
@@ -48,56 +50,81 @@ function App() {
 
   function onLimitByChange(ev) {
     // Will be used by challenge 5
+    //console.log("filtering animals by tupe....");
     const value = ev.target.value;
-    console.log('onLimitByChange', value);
-    if (value === 'all') {
+    console.log("onLimitByChange", value);
+    if (value === "all") {
       setLimitBy(false); // if "all" is selected, set to false
-    } else { // otherwise, just set with value
+    } else {
+      // otherwise, just set with value
       setLimitBy(value);
     }
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Petting Zoo Tracker</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Petting Zoo Tracker</h1>
         {/* Challenge 4 can go here ----v */}
 
         <label>
-          Search: <input value={searchTerm}
-            onChange={ev => setSearchTerm(ev.target.value)} />
+          Search:{" "}
+          <input
+            value={searchTerm}
+            onChange={(ev) => setSearchTerm(ev.target.value)}
+          />
         </label>
 
-        <img src="https://i.imgur.com/HzafyBAb.jpg" alt="cow" />
+        <label>
+          {" "}
+          Only show:
+          <select onChange={onLimitByChange} value={limitBy}>
+            <option value='all'>all</option>
+            {animalTypes.map((anm) => (
+              <option value={anm.type}>{anm.type}</option>
+            ))}
+          </select>
+        </label>
       </header>
 
-      <div className="AnimalForm">
-        <div className="AnimalForm-animalSelector">
-          {
-            animalTypes.map(animalType => (
-              <label key={animalType.type}>
-                <input type="radio" value={animalType.type}
-                  checked={newAnimalInfo.type === animalType.type}
-                  onChange={() => setNewAnimalInfo(animalType)}
-                />
-                <img src={animalType.imageSrc} alt="Animal" />
-              </label>
-            ))
-          }
+      <div className='AnimalForm'>
+        <div className='AnimalForm-animalSelector'>
+          {animalTypes.map((animalType) => (
+            <label key={animalType.type}>
+              <input
+                type='radio'
+                value={animalType.type}
+                checked={newAnimalInfo.type === animalType.type}
+                onChange={() => setNewAnimalInfo(animalType)}
+              />
+              <img src={animalType.imageSrc} alt='Animal' />
+            </label>
+          ))}
         </div>
         <input
-          placeholder="Name your animal"
+          placeholder='Name your animal'
           value={name}
           onChange={onNameChange}
         />
         <button onClick={addAnimal}>Add Animal</button>
       </div>
 
-      <div className="App-body">
-        {/* Challenge #2 can go here ---v */}
-
+      <div className='App-body'>
+        {
+          animals
+            // this first filter says... include animal IF limitBy is not set OR limitBy matches the animals type
+            .filter((anm) => limitBy === false || anm.type === limitBy)
+            .filter((anm) => anm.name.includes(searchTerm))
+            .map((anm) => (
+              <div className='Animal'>
+                <img src={anm.image} alt='Animal' />
+                <p>
+                  {anm.name} the {anm.type}
+                </p>
+              </div>
+            )) /* Challenge #2 can go here ---v */
+        }
       </div>
-
     </div>
   );
 }
