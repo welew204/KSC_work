dc36a04583acaffd993e51103b9b571d19ef9204
Welew <will@willbelew.com>
2023-12-01 09:53:10 -0800

    done thru FE5.2.2

--- frontend/5.2-redux/activities/2_brello_redux/src/components/pages/Dashboard/Dashboard.js
+++ frontend/5.2-redux/activities/2_brello_redux/src/components/pages/Dashboard/Dashboard.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { actions } from '../../../reducers/tasks.js';
-import './Dashboard.css';
+import React from "react";
+import { connect } from "react-redux";
+import { actions } from "../../../reducers/tasks.js";
+import "./Dashboard.css";
 
-import Task from '../../Task/Task.js';
+import Task from "../../Task/Task.js";
 
 function Dashboard(props) {
-
   function moveTaskRight(taskId) {
     const action = actions.moveTaskForward(taskId);
 
@@ -25,84 +24,99 @@ function Dashboard(props) {
   }
 
   function setTaskAsCompleted(taskId) {
+    console.log("Trying to complete: ", taskId);
+    const action = actions.markAsComplete(taskId);
+    props.dispatch(action);
     // TODO: Challenge 5, finish this
+  }
+
+  function copyTask(taskId) {
+    const action = actions.copyTask(taskId);
+    props.dispatch(action);
   }
 
   // "props.tasks" is how we are "getting" the redux state (see mapStateToProps
   // below)
-  const tasksPhase0 = props.tasks.filter(task => task.phase === 0);
-  const tasksPhase1 = props.tasks.filter(task => task.phase === 1);
-  const tasksPhase2 = props.tasks.filter(task => task.phase === 2);
-  const tasksPhase3 = props.tasks.filter(task => task.phase === 3);
+  const tasksPhase0 = props.tasks.filter((task) => task.phase === 0);
+  const tasksPhase1 = props.tasks.filter((task) => task.phase === 1);
+  const tasksPhase2 = props.tasks.filter((task) => task.phase === 2);
+  const tasksPhase3 = props.tasks.filter((task) => task.phase === 3);
 
   // Use a trick with "map" and "reduce" to sum up points for each column
   // (Solution to Bonus Challenge #3 of Activity 1)
-  const sum0 = tasksPhase0.map(task => Number(task.points)).reduce((a, b) => a + b, 0);
-  const sum1 = tasksPhase1.map(task => Number(task.points)).reduce((a, b) => a + b, 0);
-  const sum2 = tasksPhase2.map(task => Number(task.points)).reduce((a, b) => a + b, 0);
-  const sum3 = tasksPhase3.map(task => Number(task.points)).reduce((a, b) => a + b, 0);
+  const sum0 = tasksPhase0
+    .map((task) => Number(task.points))
+    .reduce((a, b) => a + b, 0);
+  const sum1 = tasksPhase1
+    .map((task) => Number(task.points))
+    .reduce((a, b) => a + b, 0);
+  const sum2 = tasksPhase2
+    .map((task) => Number(task.points))
+    .reduce((a, b) => a + b, 0);
+  const sum3 = tasksPhase3
+    .map((task) => Number(task.points))
+    .reduce((a, b) => a + b, 0);
 
   return (
-    <div className="Dashboard">
-      <div className="Dashboard-column">
+    <div className='Dashboard'>
+      <div className='Dashboard-column'>
         <h1>Wanna do it ({sum0})</h1>
-        {
-          tasksPhase0.map(task => (
-            <Task
-              title={task.title}
-              text={task.text}
-              phase={task.phase}
-              points={task.points}
-              id={task.id}
-              key={task.id}
-              onDelete={() => deleteTask(task.id)}
-              onMoveForward={() => moveTaskRight(task.id)}
-              onMarkAsComplete={() => setTaskAsCompleted(task.id)}
-            />
-          ))
-        }
+        {tasksPhase0.map((task) => (
+          <Task
+            title={task.title}
+            text={task.text}
+            phase={task.phase}
+            points={task.points}
+            id={task.id}
+            key={task.id}
+            onDelete={() => deleteTask(task.id)}
+            onMoveForward={() => moveTaskRight(task.id)}
+            onMarkAsComplete={() => setTaskAsCompleted(task.id)}
+            onCopy={() => copyTask(task.id)}
+          />
+        ))}
       </div>
-      <div className="Dashboard-column">
+      <div className='Dashboard-column'>
         <h1>Gettin it done ({sum1})</h1>
         {/* The "..." is shorthand to assign everything as props */}
-        {
-          tasksPhase1.map(task => (
-            <Task {...task}
-              key={task.id}
-              onDelete={() => deleteTask(task.id)}
-              onMoveForward={() => moveTaskRight(task.id)}
-              onMoveBackward={() => moveTaskLeft(task.id)}
-              onMarkAsComplete={() => setTaskAsCompleted(task.id)}
-            />
-          ))
-        }
+        {tasksPhase1.map((task) => (
+          <Task
+            {...task}
+            key={task.id}
+            onDelete={() => deleteTask(task.id)}
+            onMoveForward={() => moveTaskRight(task.id)}
+            onMoveBackward={() => moveTaskLeft(task.id)}
+            onMarkAsComplete={() => setTaskAsCompleted(task.id)}
+            onCopy={() => copyTask(task.id)}
+          />
+        ))}
       </div>
-      <div className="Dashboard-column">
+      <div className='Dashboard-column'>
         <h1>I did it already ({sum2})</h1>
-        {
-          tasksPhase2.map(task => (
-            <Task {...task}
-              key={task.id}
-              onDelete={() => deleteTask(task.id)}
-              onMoveForward={() => moveTaskRight(task.id)}
-              onMoveBackward={() => moveTaskLeft(task.id)}
-              onMarkAsComplete={() => setTaskAsCompleted(task.id)}
-            />
-          ))
-        }
+        {tasksPhase2.map((task) => (
+          <Task
+            {...task}
+            key={task.id}
+            onDelete={() => deleteTask(task.id)}
+            onMoveForward={() => moveTaskRight(task.id)}
+            onMoveBackward={() => moveTaskLeft(task.id)}
+            onMarkAsComplete={() => setTaskAsCompleted(task.id)}
+            onCopy={() => copyTask(task.id)}
+          />
+        ))}
       </div>
-      <div className="Dashboard-column">
+      <div className='Dashboard-column'>
         <h1>Forget about it ({sum3})</h1>
-        {
-          tasksPhase3.map(task => (
-            <Task {...task}
-              key={task.id}
-              onDelete={() => deleteTask(task.id)}
-              onMoveBackward={() => moveTaskLeft(task.id, 2)}
-              onMarkAsComplete={() => setTaskAsCompleted(task.id)}
-            />
-          ))
-        }
+        {tasksPhase3.map((task) => (
+          <Task
+            {...task}
+            key={task.id}
+            onDelete={() => deleteTask(task.id)}
+            onMoveBackward={() => moveTaskLeft(task.id, 2)}
+            onMarkAsComplete={() => setTaskAsCompleted(task.id)}
+            onCopy={() => copyTask(task.id)}
+          />
+        ))}
       </div>
     </div>
   );
@@ -110,5 +124,5 @@ function Dashboard(props) {
 
 // "mapStateToProps" is boilerplate for react-redux that exposes a Redux state
 // slice in our react component as a prop
-const mapStateToProps = state => ({ tasks: state });
+const mapStateToProps = (state) => ({ tasks: state });
 export default connect(mapStateToProps)(Dashboard);
--- frontend/5.2-redux/activities/2_brello_redux/src/reducers/tasks.js
+++ frontend/5.2-redux/activities/2_brello_redux/src/reducers/tasks.js
@@ -1,31 +1,37 @@
 // Reducers for Tasks "slice"
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-const {actions, reducer} = createSlice({
-  name: 'tasks',
+const { actions, reducer } = createSlice({
+  name: "tasks",
   initialState: [
     {
-      title: 'Make nice React app',
+      title: "Make nice React app",
       points: 8,
       phase: 1,
       id: 1,
-      text: 'This React thing seems good for UI, lets use it',
+      text: "This React thing seems good for UI, lets use it",
     },
     {
-      title: 'Create CRUD functionality',
+      title: "Create CRUD functionality",
       points: 5,
       phase: 3,
       id: 2,
-      text: 'Work on Python bottle.py-based CRUD API',
+      text: "Work on Python bottle.py-based CRUD API",
     },
     {
       title: "Add in Redux",
       points: 10,
       phase: 0,
       id: 3,
-      text: 'Refactor to add in this trendy thing called redux',
+      text: "Refactor to add in this trendy thing called redux",
     },
-    // TODO Challenge #4 goes here
+    {
+      title: "Learn MERN",
+      points: 8,
+      phase: 0,
+      id: 4,
+      text: "A lot of coders seem to like it",
+    },
   ],
 
   // Define the different action types, and the reducers that handle them
@@ -39,7 +45,7 @@ const {actions, reducer} = createSlice({
       // (.find is a built-in method n JS that can be used on any array and
       // will return the first item that matches the given condition, supplied
       // by the (arrow) function.)
-      const task = state.find(task => task.id === action.payload);
+      const task = state.find((task) => task.id === action.payload);
       if (task.phase > 0) {
         // Modify the task, decreasing the phase, but only if its > 0
         task.phase--;
@@ -48,7 +54,7 @@ const {actions, reducer} = createSlice({
 
     editTask: (state, action) => {
       // Use .find to search through the array looking for the task of given ID
-      const task = state.find(task => task.id === action.payload.id);
+      const task = state.find((task) => task.id === action.payload.id);
       // Modify the task with the new data
       task.title = action.payload.title;
       task.text = action.payload.text;
@@ -58,7 +64,7 @@ const {actions, reducer} = createSlice({
     deleteTask: (state, action) => {
       // Use .findIndex to search through the array looking for the index of
       // the task that matches the condition
-      const index = state.findIndex(task => task.id === action.payload);
+      const index = state.findIndex((task) => task.id === action.payload);
       state.splice(index, 1); // delete the item
     },
 
@@ -79,7 +85,7 @@ const {actions, reducer} = createSlice({
 
     addTask: (state, action) => {
       // Figure out the next available ID
-      const allIds = state.map(task => task.id);
+      const allIds = state.map((task) => task.id);
       const nextID = Math.max(...allIds) + 1;
 
       // With push, append another task to the end of the array
@@ -94,10 +100,28 @@ const {actions, reducer} = createSlice({
 
     markAsComplete: (state, action) => {
       // TODO (Challenge #5)
+      //console.log(action);
+      const task = state.find((task) => task.id === action.payload);
+      //console.log("found the task!", task);
+      task.phase = 3;
+    },
+
+    copyTask: (state, action) => {
+      // Challenge #6
+      //console.log(action);
+      const allIds = state.map((task) => task.id);
+      const nextID = Math.max(...allIds) + 1;
+
+      const task = state.find((task) => task.id === action.payload);
+
+      //console.log(task);
+      const copied_task = { ...task, phase: 0, id: nextID };
+      //console.log("copied version....", copied_task);
+      state.push(copied_task);
     },
 
     // TODO (Challenge #6 goes here)
   },
 });
 
-export {actions, reducer};
+export { actions, reducer };
