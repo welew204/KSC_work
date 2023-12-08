a670eac5d1b6e62e8c98142c6fddc8c1844586c7
Welew <will@willbelew.com>
2023-12-08 08:38:51 -0800

    FE 5.3.3 testing NEM api routes

--- frontend/5.3-mern/activities/3_express_mongo/server.js
+++ frontend/5.3-mern/activities/3_express_mongo/server.js
@@ -1,6 +1,6 @@
-// Boilerplate importing and setting up 
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+// Boilerplate importing and setting up
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 app.use(express.json());
 
@@ -8,19 +8,20 @@ app.use(express.json());
 // Configuration
 // NOTE: You will need to change this to use your MongoDB Atlas Cluster
 // Try this with one you did for activity 1.
-const MONGODB_URL = 'mongodb://kcuser:mongoerrday1@ds219432.mlab.com:19432/kcsandbox';
-const MONGODB_DATABASE = 'classactivity';
+const MONGODB_URL =
+  "mongodb+srv://dbuser:UOVfndEctLfrKY1Y@ksccluster.5akj6ym.mongodb.net/";
+const MONGODB_DATABASE = "classactivity";
 const PORT = 3000;
 /////////////////////////////////////////////
 
-
-
 // GET for getting any number of existing documents
-app.get('/api/mongodb/:collectionName/', (request, response) => {
+app.get("/api/mongodb/:collectionName/", (request, response) => {
   const collectionName = request.params.collectionName;
 
   // Get GET params
+  //console.log(request);
   const query = request.query || {};
+  console.log(query);
   db.collection(collectionName)
     .find(query)
     .toArray((err, results) => {
@@ -30,33 +31,32 @@ app.get('/api/mongodb/:collectionName/', (request, response) => {
     });
 });
 
-
 // POST, creating a new document
-app.post('/api/mongodb/:collectionName/', (request, response) => {
+app.post("/api/mongodb/:collectionName/", (request, response) => {
   const collectionName = request.params.collectionName;
   const data = request.body;
 
-  db.collection(collectionName)
-    .insert(data, (err, results) => {
-      if (err) throw err;
+  db.collection(collectionName).insertOne(data, (err, results) => {
+    if (err) throw err;
 
-      // Got data back... send to client
-      response.json({
-        'success': true,
-        'results': results,
-      });
+    // Got data back... send to client
+    response.json({
+      success: true,
+      results: results,
     });
+  });
 });
 
-
 // PUT, modify an existing document
-app.put('/api/mongodb/:collectionName/', (request, response) => {
+app.put("/api/mongodb/:collectionName/", (request, response) => {
   const collectionName = request.params.collectionName;
   const data = request.body;
   const query = request.query;
 
-  db.collection(collectionName)
-    .update(query, {$set: data}, (err, results) => {
+  db.collection(collectionName).update(
+    query,
+    { $set: data },
+    (err, results) => {
       if (err) throw err;
 
       // If we modified exactly 1, then success, otherwise failure
@@ -69,42 +69,30 @@ app.put('/api/mongodb/:collectionName/', (request, response) => {
           success: false,
         });
       }
-    });
+    }
+  );
 });
 
-
 // DELETE, delete a single document with given criteria
-app.delete('/api/mongodb/:collectionName/', (request, response) => {
+app.delete("/api/mongodb/:collectionName/", (request, response) => {
   const collectionName = request.params.collectionName;
   const query = request.query;
 
-  db.collection(collectionName)
-    .deleteOne(query, (err, results) => {
-      if (err) throw err;
+  db.collection(collectionName).deleteOne(query, (err, results) => {
+    if (err) throw err;
 
-      // If we deleted exactly 1, then success, otherwise failure
-      if (results.result.n === 1) {
-        response.json({
-          success: true,
-        });
-      } else {
-        response.json({
-          success: false,
-        });
-      }
-    })
+    // If we deleted exactly 1, then success, otherwise failure
+    if (results.result.n === 1) {
+      response.json({
+        success: true,
+      });
+    } else {
+      response.json({
+        success: false,
+      });
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
 
 // Some boilerplate here, don't worry too much about this.
 // 1. Connect to the MongoDB
@@ -126,5 +114,5 @@ MongoClient.connect(MONGODB_URL, (err, client) => {
 
   app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
-  })
+  });
 });
