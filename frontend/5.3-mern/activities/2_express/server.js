0865dda5038d3da61adbf86dd7f499918f7db052
Welew <will@willbelew.com>
2023-12-07 16:14:15 -0800

    through FE 5.3.2

--- frontend/5.3-mern/activities/2_express/server.js
+++ frontend/5.3-mern/activities/2_express/server.js
@@ -1,29 +1,37 @@
 // See instructions.md for challenge instructions
 
-// Boilerplate importing and setting up 
-const express = require('express');
+// Boilerplate importing and setting up
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-
-
 // Making a route for 'localhost:3000/hello-world/'
-app.get('/hello-world/', (request, response) => {
-
+app.get("/api/get-pi-value/", (request, response) => {
   // Send back as a response JSON data
   response.json({
-    hello: 'world',
+    pi: 3.1415926535,
   });
 });
 
+app.get("/api/esperanto/:englishWord", (request, response) => {
+  const englishWord = request.params.englishWord;
 
+  const translations = {
+    hello: "saluton",
+    goodbye: "adiau",
+    candy: "dolcajo",
+  };
 
-
-
-
-
-
-
+  if (englishWord in translations) {
+    response.json({
+      translation: translations[englishWord],
+    });
+  } else {
+    response.json({
+      error: "Unknown word",
+    });
+  }
+});
 
 //////////////////////////////////////////
 // The following is boilerplate:
@@ -40,4 +48,4 @@ const PORT = 3000;
 app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`Listening at http://localhost:${PORT}/`);
-})
+});
